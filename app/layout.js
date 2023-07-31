bf24e8907586045462b9e2e75c9645d5795effdb
carlsondultra <carlsondultra@users.noreply.github.com>
2023-07-31 18:45:32 -0400

    adding css to navbar

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import './globals.css'
 import { Inter } from 'next/font/google'
 
@@ -17,9 +18,11 @@ export default function RootLayout({ children }) {
       <body className={'min-h-screen flex flex-col relative ' + inter.className }>
         <header className='sticky top-0 p-6 bg-white border-b
         border-solid border-blue-900 shadow-md z-50 text-2xl sm:-3xl
-        md:text-4xl sm:p-8'>
-          <h1>My Store</h1>
-          <i class="fa-solid fa-cart-shopping"></i>
+        md:text-4xl sm:p-8 flex item-center justify-between'>
+          <Link href={'/'}>
+            <h1 className='uppercase cursor-pointer hover:scale-110'>My Store</h1>
+          </Link>
+          <i class="fa-solid cursor-pointer hover:text-slate-500 fa-cart-shopping"></i>
         </header>
         <div className='flex-1'>
           {children}
--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className=''>
+    <main className='p-4'>
       tetstettesttestestest
     </main>
   )
