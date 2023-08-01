5237354377af959941a3d06c2961bab37502ea3d
carlsondultra <carlsondultra@users.noreply.github.com>
2023-07-31 20:14:15 -0400

    adding stripe integration, adding products

--- app/page.js
+++ app/page.js
@@ -1,6 +1,20 @@
 import Image from 'next/image'
+import Stripe from 'stripe'
 
-export default function Home() {
+async function getStripeProducts() {
+  const stripe = new Stripe(process.env.STRIPE_SECRET ?? '', {
+    apiVersion: '2020-08-27'
+  })
+  const res = await stripe.prices.list({
+    expand: ['data.product']
+  })
+  const prices = res.data
+  return prices 
+}
+
+export default async function Home() {
+  const products = await getStripeProducts()
+  console.log(products)
   return (
     <main className='p-4'>
       tetstettesttestestest
