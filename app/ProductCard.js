099c48d1fbcc6421b2c57ccfc19c983d972c0006
carlsondultra <carlsondultra@users.noreply.github.com>
2023-08-01 15:21:40 -0400

    implementing routing to product page when clicking product

--- app/ProductCard.js
+++ app/ProductCard.js
@@ -1,12 +1,21 @@
+"use client"
 import React from 'react'
+import { useRouter } from 'next/navigation'
 
 export default function ProductCard(props) {
     const {product} = props
     const {id: price_id, unit_amount: cost, product: productInfo} = product
     const {name, description} = productInfo
+
+    const router = useRouter()
+
+    function onProductClick() {
+        router.push('/product?price_id=' + price_id)
+    }
+
   return (
-    <div className='flex flex-col shadow bg-white hover:shadow-lg
-    cursor-point'>
+    <div onClick={onProductClick} className='flex flex-col shadow bg-white hover:shadow-lg
+    cursor-pointer'>
         <img src={productInfo.images[0]} alt={name} className="w-full
         h-full object-cover"/>
         <div className='flex flex-col gap-2 p-4'>
---
+++ app/product/page.js
@@ -0,0 +1,10 @@
+export default function ProductPage(props) {
+    const {searchParams} = props
+    const {price_id} = props
+    console.log(searchParams)
+    return (
+        <div>
+            THIS IS THE PRODUCT PAGE!
+        </div>
+    )
+}
