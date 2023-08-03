b8a75e143464acf0ec36501dd4846779d7d3c07a
carlsondultra <carlsondultra@users.noreply.github.com>
2023-08-03 18:28:58 -0400

    added cart modal functionality when opening and closing

--- app/Header.js
+++ app/Header.js
@@ -7,6 +7,8 @@ import Modal from './Modal'
 export default function Header() {
   const cartItems = useCart(state => state.cart)
   const openModal = useCart(state => state.openModal)
+  const setOpenModal = useCart(state => state.setOpenModal)
+
   console.log(cartItems)
   return (
     <header className='sticky top-0 p-6 bg-white border-b
@@ -18,7 +20,7 @@ export default function Header() {
         <Link href={'/'}>
             <h1 className='uppercase cursor-pointer hover:scale-110'>My Store</h1>
         </Link>
-        <div className='relative cursor-pointer group grid place-items-center'>
+        <div onClick={setOpenModal} className='relative cursor-pointer group grid place-items-center'>
           {cartItems.length > 0 && (
             <div className='absolute aspect-square pointer-events-none h-5 sm:h-6 grid place-items-center top-0 
             bg-blue-400 text-white rounded-full right-0 -translate-y-1/2
--- app/Modal.js
+++ app/Modal.js
@@ -1,7 +1,10 @@
 "use client"
 import React from 'react'
 import ReactDom from 'react-dom'
+import useCart from './(store)/store'
 export default function Modal() {
+    const closeModal = useCart(state => state.setOpenModal)
+
 
     const [domReady, setDomReady] = React.useState(false)
 
@@ -11,12 +14,13 @@ export default function Modal() {
 
     return domReady ? ReactDom.createPortal(
         <div className='fixed top-0 left-0 w-screen h-screen z-50'>
-            <div className='bg-transparent absolute inset-0'></div>
+            <div onClick={closeModal} className='bg-transparent absolute inset-0'></div>
             <div className='flex flex-col bg-white absolute right-0 top-0 h-screen shadow-lg 
             w-screen sm:w-96 max-w-screen gap-4'>
                 <div className='flex items-center p-6 justify-between text-xl relative'>
                     <h1>Cart</h1>
-                    <i className="fa-solid fa-xmark"></i>
+                    <i onClick={closeModal} className="fa-solid cursor-pointer hover:opacity-60
+                    fa-xmark"></i>
                     <div className='absolute bottom-0 left-1/2 -translate-x-1/2 h-[1px] 
                     bg-slate-300 w-2/3'></div>
                 </div>
