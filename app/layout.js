ec775a4a2900ae35e12dffac5f449914956392db
carlsondultra <carlsondultra@users.noreply.github.com>
2023-08-07 21:31:27 -0400

    adding github repo link on footer

--- app/layout.js
+++ app/layout.js
@@ -23,8 +23,8 @@ export default function RootLayout({ children }) {
         </div>
         <footer className='flex items-center flex-wrap justify-center 
         border-t border-solid border-slate-300 p-4 md:p-8'>
-          <Link href={'https://www.instagram.com'} target="_blank">
-            <i className="fa-brands fa-instagram text-slate-700
+          <Link href={'https://github.com/carlsondultra/nextjs-store'} target="_blank">
+            <i className="fa-brands fa-github text-slate-700
             hover:text-slate-500 cursor-pointer text-2xl sm:text-3xl
             md:text-4xl"></i>
           </Link>
